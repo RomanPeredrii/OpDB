379ab3cdd54a47cb28d123cfc4645a799c7da730
RomanPeredrii <42037680+RomanPeredrii@users.noreply.github.com>
2019-05-17 17:01:15 +0300

    add log page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+let fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  fs.readFile('/frontopdb/build/index.html', 'utf-8', (err, file) => {
+    if (err) return res.send('failed reading of page')
+    return res.send(file);
+  });
+
+
+
 });
 
 module.exports = router;
