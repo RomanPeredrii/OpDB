5078fdd7f08d83ccfd948de60bb2d2352f8e4741
RomanPeredrii <42037680+RomanPeredrii@users.noreply.github.com>
2019-07-02 19:34:35 +0300

    some changing

--- front-opdb/src/components/Context-menu/Context-menu.js
+++ front-opdb/src/components/Context-menu/Context-menu.js
@@ -2,15 +2,29 @@ import React, { useRef } from 'react';
 import './Context-menu.css';
 import { log, goTo } from '../../my_modules/stuff/index';
 
+
 function ContextMenu(props) {
-  
+
+// log(props);
+
+  const closeContextMenu = () => {
+    const close = new Event("close");
+    window.dispatchEvent(close);
+  };
+  const editItem = () => {log(props.position.cont)
+    const edit = new Event("edit");
+    window.dispatchEvent(edit);
+    closeContextMenu();
+  }; 
   return (
-    <div className="ContextMenu">
+    <div className="ContextMenu" style={props.position} >
+      <div className="Substrate" onClick={closeContextMenu} ></div>
       <table>
-        <tr>Add {props.state}</tr>
-        <tr>Edit</tr>
-        <tr>Remove</tr>
+        <tr onClick={editItem}>Add </tr>
+        <tr onClick={editItem}>Edit</tr>
+        <tr onClick={editItem}>Remove</tr>
       </table>
+
     </div>
   );
 }
--- front-opdb/src/components/Main-table/Main-table.js
+++ front-opdb/src/components/Main-table/Main-table.js
@@ -1,20 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Main-table.css';
 import ContextMenu from '../Context-menu/Context-menu';
-import {log, headers, goTo} from '../../my_modules/stuff/index';
+import { log, headers, goTo } from '../../my_modules/stuff/index';
 
 
-const check = () => {return {a:3}} ;
 
 function MainTable() {
-  const [context, setContext] = useState({a:3, b:6, v:0});
-  
-  const showMenu = (e) => {
-    const clickX = e.clientX;
-    const clickY = e.clientY;
+  const [contextEvent, setContextMenu] = useState({});
+  const [openedMenu, setOpenedMenu] = useState(false);
+  const [checkClose, setClose] = useState(false);
+  const [checkEditInput, setEditInput] = useState(false);
+  const [editInput, setOpenedEditInput] = useState(false);
 
+  const contextMenu = (e, row) => {
+    log(e.clientX, e.clientY);
+    e.preventDefault();
+    setContextMenu({ left: e.clientX, top: e.clientY, cont: row });
+    setOpenedMenu(true);
+  };
 
-  }
+  const close = () => { window.addEventListener("close", () => setOpenedMenu(false), false) };
+  const edit = () => {
+    window.addEventListener("edit", () => {
+      setOpenedEditInput(true)
+      //log('edit event:   ', edit);
+    }, false)
+  };
+
+  useEffect(() => {
+    if (!checkClose) close();
+    setClose(true);
+  });
+
+  useEffect(() => {
+    log('36 useEffect() setEditInput', checkEditInput, editInput);
+    if (!checkEditInput) edit();
+    log('38 useEffect() ?checkEditInput setEditInput', checkEditInput, editInput);
+
+    setEditInput(true);
+  });
 
   return (
     <div className="MainTable">
@@ -30,7 +54,6 @@ function MainTable() {
               'customs',
               'line',
               'vehicle'].map((row) =>
-
                 <th>
                   {row}
                 </th>
@@ -39,7 +62,7 @@ function MainTable() {
         </thead>
         <tbody>
           <tr>
-            {[  'HLXU8447492',
+            {['HLXU8447492',
               'HLCUMTR190414386',
               'TE-STAR',
               'ODESSA',
@@ -48,16 +71,24 @@ function MainTable() {
               'weights',
               '26/06',
               ''].map((row) =>
-<td>
-                {/* <td onContextMenu={showMenu}> */}
-                  {row} 
+                <td onContextMenu={(e) => {
+                  contextMenu(e, row);
+                  log('76 EditInput', editInput);
+                  (() => {
+                    if (editInput) {log('78 ?EditInput', editInput)
+                    return <div className="EditInput">
+                      <p>EditInput</p>
+                    </div>
+                  }})()
+
+                }}>
+                  {row}
                 </td>
               )}
           </tr>
         </tbody>
-      </table>
-      <ContextMenu />
-      <p>{log(context)}</p>
+      </table >
+      {(() => { if (openedMenu) return <ContextMenu position={contextEvent} /> })()}
     </div>
   );
 }
